f82ddf064746a6ca539077cf5575a3b7fae934c8
Chenglim Ear <chenglim_ear@yahoo.com>
2023-10-17 20:16:59 +0000

    Add built in support to test downloading of files

--- src/gdrive_datastore/gdrive.py
+++ src/gdrive_datastore/gdrive.py
@@ -4,6 +4,21 @@ from oauth2client.service_account import ServiceAccountCredentials
 from pydrive2.auth import GoogleAuth 
 from pydrive2.drive import GoogleDrive
 
+def test_data_pull(default_folder='', default_subfolder='_LOCAL_'):
+    REPO_BRANCH = os.getenv('REPO_BRANCH',default_subfolder)
+    GDRIVE_FOLDER = os.getenv('GDRIVE_FOLDER',default_folder) or default_folder
+
+    downloads_dir = '.local/downloads'
+    os.makedirs(downloads_dir, exist_ok=True)
+    copier = GDriveCopier(GDRIVE_FOLDER, target_branch = REPO_BRANCH)
+    copier.download_to(downloads_dir)
+    print(f'Contents of downloads dir ({downloads_dir}):')
+    local_files = os.listdir(downloads_dir)
+    for local_file in local_files:
+        print(local_file)
+    print('Done')
+
+
 class GDriveCopier:
     '''This copier class supports uploading and downloading files between a local folder
 and a folder on Google Drive.  
